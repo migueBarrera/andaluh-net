9bcf2bf04b3fa175f2a17f72d34da7f222600021
Miguel Ángel Barrera Muñoz <mangel@plainconcepts.com>
2019-04-04 16:44:45 +0200

    Rules added , Test App added

--- Andaluh/EPA.cs
+++ Andaluh/EPA.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace Andaluh
 {
-    public class EPA
+    public static class EPA
     {
-        public static string Transcribe(string text)
+        public static string Transcribe(string text, bool debug = false)
         {
             //Do not start transcription if the input is empty
             if (string.IsNullOrWhiteSpace(text))
@@ -14,7 +14,32 @@ namespace Andaluh
                 return string.Empty;
             }
 
-            return text;
+
+            return EPARules.VRules(text);
+        }
+
+        public static string ToAndaluh(this string text, bool debug = false)
+        {
+            //Do not start transcription if the input is empty
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return string.Empty;
+            }
+
+            var resH = EPARules.HRules(text);
+
+            var resX = EPARules.XRules(resH);
+
+            var resCH = EPARules.CHRules(resX);
+
+            var resV = EPARules.VRules(resCH);
+
+            var resLL = EPARules.LLRules(resV);
+
+            var resL = EPARules.LRules(resLL);
+
+
+            return resL;
         }
     }
 }
---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Andaluh;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Andaluh.EPA.Transcribe("Llavero");
+            var newREsult = "Esto es una prueba de transcripción con el modo debug activo para ver cómo va cambiando la frase cada regla".ToAndaluh();
+            Console.WriteLine("------------------RESULT=>"+ newREsult);
+        }
+    }
+}
