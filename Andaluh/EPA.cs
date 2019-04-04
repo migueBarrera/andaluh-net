9702d54b6a2f7ce7ed59dd390a8227ecca7f6a20
Miguel Ángel Barrera Muñoz <mangel@plainconcepts.com>
2019-04-04 12:29:18 +0200

    Add project files.

---
+++ Andaluh/EPA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andaluh
+{
+    public class EPA
+    {
+        public static string Transcribe(string text)
+        {
+            //Do not start transcription if the input is empty
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return string.Empty;
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Andaluh/EPARules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andaluh
+{
+    /// <summary>
+    /// Supress mute /h/
+    /// </summary>
+    internal class EPARules
+    {
+        internal static string XRules(string text)
+        {
+            //todo add chihuahua
+
+            return "";
+        }
+    }
+}
